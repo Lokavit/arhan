6fe4e81f518ea51b484f6389003ad4793a87d326
Lokavit <491262164@qq.com>
2024-12-07 12:47:48 +0800

    -c init

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import config from "./src/config";
 import lgre from "./src/lgre";
-import { fmt } from "./src/fmt/index";
+import { fmtText } from "./src/fmt/text";
+import { fmtHMS } from "./src/fmt/date";
+import {
+  fmtPercent,
+  fmtNumber,
+  fmtCrypto,
+  fmtAmount,
+  fmtQuantity,
+} from "./src/fmt/number";
 
 export { config };
 export { lgre };
-export { fmt };
+export { fmtText };
+export { fmtHMS };
+export { fmtPercent };
+export { fmtNumber };
+export { fmtCrypto };
+export { fmtAmount };
+export { fmtQuantity };
